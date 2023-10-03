2dbbcae8706f6e5b3df1a1a91bdc97c9bb8fdb5a
Osvy1994 <osvaldo.elvirez@gmail.com>
2023-10-03 09:43:52 -0400

    adding metadata

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Header } from "tar";
 import "./App.css";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      <Header />
+      <LandingPage />
+      <Menu />
+      <Testimonials />
+      <AboutUs />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
